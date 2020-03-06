73973d181090197e7fee068927bf83a6c5f2a259
LeuAlmeida <leo@webid.net.br>
2020-03-06 13:48:56 -0300

    feat(routes): implementing Header into router file

--- web/src/routes.js
+++ web/src/routes.js
@@ -5,9 +5,12 @@ import Home from './pages/Home';
 import Coronavirus from './pages/Coronavirus';
 import SP500 from './pages/Stocks/SP500';
 
+import Header from './components/Header';
+
 export default function Routes() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/coronavirus" component={Coronavirus} />
