a949e00c117bf2ce58dfc2d6202af1634632353e
LeuAlmeida <leo@webid.net.br>
2020-03-06 14:19:19 -0300

    feat(Header): create animation into component

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { Animated } from 'react-animated-css';
+
 import { Container, Logo, Nav, NavItem } from './styles';
 
 import logo from '../../assets/logo.svg';
@@ -11,17 +13,29 @@ export default function Header() {
       <Nav>
         <Logo>
           <Link to="/">
-            <img src={logo} alt="Logo" />
+            <Animated
+              animationIn="bounceInLeft"
+              animationOut="fadeOut"
+              isVisible
+            >
+              <img src={logo} alt="Logo" />
+            </Animated>
           </Link>
         </Logo>
         <NavItem>
-          <Link to="/stocks/sp500">S&P500</Link>
-          <Link to="/stocks/ibov">Ibovespa</Link>
-          <Link to="/stocks/ifix">IFIX</Link>
-          <Link to="/stocks/nyse">NYSE</Link>
-          <Link to="/stocks/nasdaq">NASDAQ</Link>
-          <Link to="/stocks/gold">Gold</Link>
-          <Link to="/stocks/usd">USD to BRL</Link>
+          <Animated
+            animationIn="bounceInRight"
+            animationOut="fadeOut"
+            isVisible
+          >
+            <Link to="/stocks/sp500">S&P500</Link>
+            <Link to="/stocks/ibov">Ibovespa</Link>
+            <Link to="/stocks/ifix">IFIX</Link>
+            <Link to="/stocks/nyse">NYSE</Link>
+            <Link to="/stocks/nasdaq">NASDAQ</Link>
+            <Link to="/stocks/gold">Gold</Link>
+            <Link to="/stocks/usd">USD to BRL</Link>
+          </Animated>
         </NavItem>
       </Nav>
     </Container>
