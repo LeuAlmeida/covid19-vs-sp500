4bc882d3af29d751824ca6a402e75eb933136251
LeuAlmeida <leo@webid.net.br>
2020-03-06 13:49:19 -0300

    style(Header): create Header stylesheet

--- web/src/components/Header/styles.js
+++ web/src/components/Header/styles.js
@@ -1,3 +1,43 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  background: #fff;
+  max-height: 90px;
+  align-items: center;
+  justify-content: center;
+  padding: 5px;
+  margin-bottom: 10px;
+`;
+
+export const Nav = styled.nav`
+  max-width: 800px;
+  display: flex;
+  flex: 1;
+`;
+
+export const Logo = styled.div`
+  img {
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+export const NavItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  flex: 1;
+
+  a {
+    font-weight: bold;
+    color: #333;
+    margin: 0 10px;
+    transition: color 0.4s;
+    text-transform: uppercase;
+
+    &:hover {
+      color: #b31217;
+    }
+  }
+`;
