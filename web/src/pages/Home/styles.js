7b957f5bbc3440e0983e876b8124d7e906ec7a83
LeuAlmeida <leo@webid.net.br>
2020-03-06 14:09:00 -0300

    style(Home): improvements and new Home components

--- web/src/pages/Home/styles.js
+++ web/src/pages/Home/styles.js
@@ -1,3 +1,30 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  max-width: 900px;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
+
+export const Wrapper = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #fff;
+  padding: 20px;
+  border-radius: 5px;
+
+  h1 {
+    color: #b31217;
+    margin-bottom: 20px;
+  }
+
+  p {
+    color: #333;
+  }
+`;
