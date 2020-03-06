b6805994a2d669766fe731c8917373755016eef7
LeuAlmeida <leo@webid.net.br>
2020-03-06 13:49:32 -0300

    feat(Header): create component

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import { Container, Logo, Nav, NavItem } from './styles';
+
+import logo from '../../assets/logo.svg';
 
 export default function Header() {
-  return <div />;
+  return (
+    <Container>
+      <Nav>
+        <Logo>
+          <Link to="/">
+            <img src={logo} alt="Logo" />
+          </Link>
+        </Logo>
+        <NavItem>
+          <Link to="/stocks/sp500">S&P500</Link>
+          <Link to="/stocks/ibov">Ibovespa</Link>
+          <Link to="/stocks/ifix">IFIX</Link>
+          <Link to="/stocks/nyse">NYSE</Link>
+          <Link to="/stocks/nasdaq">NASDAQ</Link>
+          <Link to="/stocks/gold">Gold</Link>
+          <Link to="/stocks/usd">USD to BRL</Link>
+        </NavItem>
+      </Nav>
+    </Container>
+  );
 }
