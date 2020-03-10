8d71fc2e70970585fa4fc5221ce81e77000f934d
LeuAlmeida <leo@webid.net.br>
2020-03-10 12:24:13 -0300

    style(home): create CardContainer styles

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -10,6 +10,17 @@ export const Container = styled.div`
   margin: 0 auto;
 `;
 
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  max-width: 900px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
+
 export const Wrapper = styled.div`
   width: 100%;
   max-width: 900px;
