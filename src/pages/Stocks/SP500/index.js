39ba30492e73cc56a7b1189e64defb23ba086217
LeuAlmeida <leo@webid.net.br>
2020-03-10 17:54:32 -0300

    feat(SP500): improving initial structure to allow useState and useEffect (to load API)

--- src/pages/Stocks/SP500/index.js
+++ src/pages/Stocks/SP500/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-import { Container } from './styles';
+import { Container, Wrapper } from './styles';
 
 import Charts from '../../../components/Charts';
 
 export default function SP500() {
   return (
     <Container>
-      <Charts />
+      <Wrapper>
+        <Charts />
+      </Wrapper>
     </Container>
   );
 }
