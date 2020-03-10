099eb96e29335c95a105db006ff2a38692ded705
LeuAlmeida <leo@webid.net.br>
2020-03-10 17:39:23 -0300

    feat(Charts): implementing Chart as a component

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { Line } from 'react-chartjs-2';
 
-// import { Container } from './styles';
+function Charts() {
+  const lineData = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+      {
+        label: 'My First dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 80, 81, 56, 55, 40],
+      },
+      {
+        label: 'My second dataset',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,192,192,0.4)',
+        borderColor: 'rgba(75,192,192,1)',
+        borderCapStyle: 'butt',
+        borderDash: [],
+        borderDashOffset: 0.0,
+        borderJoinStyle: 'miter',
+        pointBorderColor: 'rgba(75,192,192,1)',
+        pointBackgroundColor: '#fff',
+        pointBorderWidth: 1,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+        pointHoverBorderColor: 'rgba(220,220,220,1)',
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10,
+        data: [65, 59, 12, 22, 95, 40, 55],
+      },
+    ],
+  };
 
-export default function Charts() {
-  return <div />;
+  const wrapperStyle = {
+    backgroundColor: 'white',
+    border: 'solid 1px',
+    margin: '5px',
+    padding: '10px',
+  };
+
+  return (
+    <div className="container">
+      <h2>Chartjs2</h2>
+      <div style={wrapperStyle}>
+        <h4>Line Chart</h4>
+        <Line height="100" data={lineData} />
+      </div>
+    </div>
+  );
 }
+
+export default Charts;
