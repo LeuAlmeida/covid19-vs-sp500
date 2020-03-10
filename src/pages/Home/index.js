87ca4935dcb7a2861ae93a7a51574f3a4079e1e9
LeuAlmeida <leo@webid.net.br>
2020-03-10 12:24:54 -0300

    feat(Home): craete confirmed and recovered cases

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,10 +3,12 @@ import React, { useState, useEffect } from 'react';
 import api from '../../services/api';
 
 import Card from '../../components/Card';
-import { Container, Wrapper } from './styles';
+import { Container, CardContainer, Wrapper } from './styles';
 
 function Home() {
   const [deaths, setDeaths] = useState({});
+  const [confirmed, setConfirmed] = useState({});
+  const [recovered, setRecovered] = useState({});
 
   useEffect(() => {
     async function loadDeaths() {
@@ -15,13 +17,32 @@ function Home() {
       setDeaths(response.data);
     }
 
+    async function loadCases() {
+      const response = await api.get('/confirmed');
+
+      setConfirmed(response.data);
+    }
+
+    async function loadRecovered() {
+      const response = await api.get('/recovered');
+
+      setRecovered(response.data);
+    }
+
     loadDeaths();
+    loadRecovered();
+    loadCases();
   }, []);
 
   console.log(deaths);
+
   return (
     <Container>
-      <Card>Teste</Card>
+      <CardContainer>
+        <Card title="Mortos" quantity={deaths.latest} />
+        <Card title="Confirmados" quantity={confirmed.latest} />
+        <Card title="Recuperados" quantity={recovered.latest} />
+      </CardContainer>
       <Wrapper>
         <h1>COVID-19 vs S&P500 Index</h1>
         <p>{deaths.latest}</p>
