416515a2e2e55c963f3218a577c9915786456475
LeuAlmeida <leo@webid.net.br>
2020-03-10 14:45:02 -0300

    feat(api): implementing geoUrl

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,7 @@ const api = axios.create({
   baseURL: 'https://coronavirus-tracker-api.herokuapp.com',
 });
 
+export const geoUrl =
+  'https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-110m.json';
+
 export default api;
