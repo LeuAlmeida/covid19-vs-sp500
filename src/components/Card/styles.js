886a1da39045570a383d3c7c45f6c2e2b2b2cde6
LeuAlmeida <leo@webid.net.br>
2020-03-10 11:49:51 -0300

    style(Card): create styles

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -1,3 +1,35 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  max-width: 900px;
+  margin-bottom: 20px;
+  margin-top: 20px;
+`;
+
+export const IndividualCard = styled.div`
+  display: flex;
+  width: 295px;
+  height: 40px;
+  background: #fff;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+
+  & + div {
+    margin-left: 10px;
+  }
+
+  h3 {
+  }
+
+  span {
+    color: #333;
+    align-self: center;
+    justify-self: center;
+  }
+`;
