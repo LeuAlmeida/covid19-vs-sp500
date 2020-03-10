0a5d1cdf0a7f72f4648f47e44d805b540759a0f8
LeuAlmeida <leo@webid.net.br>
2020-03-10 17:54:10 -0300

    style(SP500): improving styled component

--- src/pages/Stocks/SP500/styles.js
+++ src/pages/Stocks/SP500/styles.js
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 900px;
+  background: #fff;
+  align-items: center;
+  padding: 20px;
+  border-radius: 5px;
+  margin-bottom: 30px;
+`;
