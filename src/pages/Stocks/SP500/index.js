99291bdc3935fdf150ae5c43ffdaecd59098d2cb
LeuAlmeida <leo@webid.net.br>
2020-03-12 10:19:20 -0300

    feat(SP500): improving SP500 logical component

--- src/pages/Stocks/SP500/index.js
+++ src/pages/Stocks/SP500/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable no-restricted-syntax */
 
 import React, { useState, useEffect } from 'react';
-import { format, getMonth } from 'date-fns';
+import { format } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 
 import { Container, Wrapper } from './styles';
@@ -13,33 +13,7 @@ import Charts from '../../../components/Charts';
 
 export default function SP500() {
   const [coronaData, setCoronaData] = useState([]);
-  const [sp500Data, setSp500Data] = useState([]);
-  const [teste, setTeste] = useState([]);
-
-  const monthNames = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-  ];
-
-  const data = [];
-
-  // function getOwnPropertyNames(name) {
-  //   return Object.getOwnPropertyNames(name);
-  // }
-
-  // function getOwnPropertyContent(name) {
-  //   return Object.getOwnPropertyContent(name);
-  // }
+  // const [sp500Data, setSp500Data] = useState([]);
 
   useEffect(() => {
     async function loadCorona() {
@@ -49,36 +23,100 @@ export default function SP500() {
 
       const history = locations.map(loc => loc.history);
 
+      // console.log(locations);
+
       let key;
 
-      history.forEach(hist => {
+      let January = { cases: 0 };
+      const February = { cases: 0 };
+      const March = { cases: 0 };
+
+      history.map(hist => {
         for (key in hist) {
-          data.push({
-            [format(new Date(key), 'MMMM')]: hist[key],
-          });
+          // if (format(new Date(key), 'MMMM') === 'January') {
+          January = {
+            [key]:
+              January.cases +
+              Number(hist[Object.keys(hist)[Object.keys(hist).length - 1]]),
+          };
+          // }
+          // if (format(new Date(key), 'MMMM') === 'February') {
+          //   February = {
+          //     cases:
+          //       January.cases +
+          //       Number(hist[Object.keys(hist)[Object.keys(hist).length - 1]]),
+          //   };
+          // }
+          // if (format(new Date(key), 'MMMM') === 'March') {
+          //   March = {
+          //     cases:
+          //       January.cases +
+          //       Number(hist[Object.keys(hist)[Object.keys(hist).length - 1]]),
+          //   };
+          // }
         }
       });
 
-      // const map = data.map(item => ({
-      //   [Object.getOwnPropertyNames(item)]: {
-      //     count: Object.values(item).map(i => i),
-      //   },
-      // }));
+      console.log([January, February, March]);
 
-      setTeste(data);
+      // history.forEach(hist => {
+      //   for (key in hist) {
+      //     if (format(new Date(key), 'MMMM') === 'January') {
+      //       // January = {
+      //       //   cases: January.cases + Number(hist[Object.keys(hist.pop())]),
+      //       // };
+      //       January = {
+      //         cases:
+      //           January.cases +
+      //           Number(hist[Object.keys(hist)[Object.keys(hist).length - 1]]),
+      //       };
+      //     }
+
+      //     console.log(history);
+      //     // const data = hist.map(
+      //     //   loc =>
+      //     // {
+      //     // [format(new Date(key), 'MMMM')]: loc.history[
+      //     //   Object.keys(hist)[Object.keys(loc.history).length - 1]
+      //     // ],
+
+      //     // console.log(loc)
+      //     // }
+      //     // );
+      //     // console.log(data);
+      //     // data.map(item => {
+      //     //   if (Object.getOwnPropertyNames(item) === 'January') {
+      //     //     January = { case: January.cases + Number(hist[data]) }
+      //     //   }
+      //     // });
+      //     // if (format(new Date(key), 'MMMM') === 'January') {
+      //     //   // January = {
+      //     //   //   cases: January.cases + Number(hist[Object.keys(hist.pop())]),
+      //     //   // };
+      //     //   January = { cases: January.cases + Number(hist[data]) };
+      //     // }
+      //     // } else if (format(new Date(key), 'MMMM') === 'February') {
+      //     //   February = { cases: February.cases + Number(hist[key]) };
+      //     // } else if (format(new Date(key), 'MMMM') === 'March') {
+      //     //   March = { cases: March.cases + Number(hist[key]) };
+      //     // } else {
+      //     //   return null;
+      //     // }
+      //   }
+
+      //   // console.log(January);
+
+      //   setCoronaData([January.cases, February.cases, March.cases]);
+      // });
     }
 
     loadCorona();
   }, []);
 
-  console.log(teste);
-  // console.log(teste.map(t => Object.getOwnPropertyNames(t)));
-
   return (
     <Container>
       <Wrapper>
-        {/* <Charts /> */}
-        <Charts coronaData={coronaData} sp500Data={sp500Data} />
+        <Charts coronaData={coronaData} />
       </Wrapper>
     </Container>
   );
